919caa0632ce9af0d962570770f43c9bcabcc060
Isadora Pilau <isadorapda@gmail.com>
2023-05-11 17:36:22 +0100

    feat: 🎸 implement Repository Design and SOLID principles

---
+++ src/http/controllers/registers.ts
@@ -0,0 +1,30 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterService } from '@/services/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function registerController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerService = new RegisterService(usersRepository)
+    await registerService.service({
+      name,
+      email,
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
---
+++ src/services/register.ts
@@ -0,0 +1,30 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterServiceRequest {
+  name: string
+  email: string
+  password: string
+}
+
+// SOLID
+// D - Dependency Inversion Principle
+
+export class RegisterService {
+  constructor(private usersRepository: UsersRepository) {}
+  async service({ name, email, password }: RegisterServiceRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new Error('Email already exists')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
