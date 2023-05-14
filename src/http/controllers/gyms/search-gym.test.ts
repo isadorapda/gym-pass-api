eef34c70c4b7d8b0f419aa398fb417fe477c66d9
Isadora Pilau <isadorapda@gmail.com>
2023-05-14 21:33:12 +0100

    test: 💍 add E2E tests gym controllers

---
+++ src/http/controllers/gyms/search-gym.test.ts
@@ -0,0 +1,53 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/create-and-authenticate-user'
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import request from 'supertest'
+
+describe('Search gym controller (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('User should be able to search gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        gym_name: 'Pure TypeScript Gym',
+        description: '',
+        phone: '0000000',
+        latitude: 51.3768971,
+        longitude: -0.2031183,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        gym_name: 'Pure JavaScript Gym',
+        description: '',
+        phone: '0000000',
+        latitude: 51.3768971,
+        longitude: -0.2031183,
+      })
+
+    const gyms = await request(app.server)
+      .get('/gyms/search')
+      .query({ query: 'TypeScript' })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(gyms.statusCode).toEqual(200)
+    expect(gyms.body.gyms).toHaveLength(1)
+    expect(gyms.body.gyms).toEqual([
+      expect.objectContaining({
+        gym_name: 'Pure TypeScript Gym',
+      }),
+    ])
+  })
+})
--- src/http/controllers/users/profile.test.ts
+++ src/http/controllers/users/profile.test.ts
@@ -1,6 +1,7 @@
 import { app } from '@/app'
 import { afterAll, beforeAll, describe, expect, test } from 'vitest'
 import request from 'supertest'
+import { createAndAuthenticateUser } from '@/utils/create-and-authenticate-user'
 
 describe('Profile Controller (E2E)', () => {
   beforeAll(async () => {
@@ -12,19 +13,7 @@ describe('Profile Controller (E2E)', () => {
   })
 
   test('User should be able to access their profile', async () => {
-    await request(app.server).post('/users').send({
-      name: 'John Doe',
-      email: 'johndoe@email.com',
-      password: '123456',
-    })
-
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: 'johndoe@email.com',
-      password: '123456',
-    })
-
-    const { token } = authResponse.body
-
+    const { token } = await createAndAuthenticateUser(app)
     const profileResponse = await request(app.server)
       .get('/me')
       .set('Authorization', `Bearer ${token}`)
