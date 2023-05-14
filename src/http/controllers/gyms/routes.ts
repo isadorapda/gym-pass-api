b0e74ea8ad429f545debeb2d18a013b58b89a501
Isadora Pilau <isadorapda@gmail.com>
2023-05-14 18:15:16 +0100

    feat: 🎸 create gyms controllers and routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { searchGymsController } from './search-gym.controller'
+import { fetchNearbyGymController } from './nearby-gym.controller'
+import { registerGymController } from './register-gym.controller'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', searchGymsController)
+  app.get('/gyms/nearby-gyms', fetchNearbyGymController)
+
+  app.post('/gyms', registerGymController)
 }
